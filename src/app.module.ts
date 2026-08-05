37865cc79ea428555a1ec39daac2f0f2b7d74d4c
javiervera123 <veraadsi123@gmail.com>
2026-08-05 11:31:49 -0500

    feat: configuracion de conexiones a MongoDB y PostgreSQL en AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb://admin:password123@localhost:27018/monolith_mongo?authSource=admin'
+    ),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'postgres',
+      password: 'password123',
+      database: 'monolith_db',
+      autoLoadEntities: true,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
